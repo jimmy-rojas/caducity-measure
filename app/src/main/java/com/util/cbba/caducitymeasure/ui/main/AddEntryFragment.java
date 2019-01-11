6b089f7d7c3242b4a8cc7194f3af54ee677af171
Jimmy Rojas <jimmy.rojas@truextend.com>
2019-01-11 13:28:40 -0400

    added new screen to add new entries

---
+++ app/src/main/java/com/util/cbba/caducitymeasure/ui/main/AddEntryFragment.java
@@ -0,0 +1,36 @@
+package com.util.cbba.caducitymeasure.ui.main;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.util.cbba.caducitymeasure.R;
+
+
+public class AddEntryFragment extends Fragment {
+
+    private AddEntryViewModel mViewModel;
+
+    public static AddEntryFragment newInstance() {
+        return new AddEntryFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.add_entry_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(AddEntryViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/util/cbba/caducitymeasure/ui/main/AddEntryViewModel.java
@@ -0,0 +1,7 @@
+package com.util.cbba.caducitymeasure.ui.main;
+
+import android.arch.lifecycle.ViewModel;
+
+public class AddEntryViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
